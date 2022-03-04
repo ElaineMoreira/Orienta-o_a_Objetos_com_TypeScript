60bae292c2cbe7350022c8fddd5ef69c6f929111
Elaine <elainefelipemoreira@gmail.com>
2022-03-04 10:56:31 -0300

    Altera saques e depositos

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
 import { ContaBancaria } from "./models/ContaBancaria";
+
 // criar uma nova instancia
 const conta1 = new ContaBancaria();
-
-/**depositar */
+/**consultar */
 let saldoAtual: number = conta1.consultar();
 console.log(`Saldo inicial: ${saldoAtual}`);
+
+/**depositar */
 conta1.depositar(50);
 saldoAtual = conta1.consultar();
-console.log(`segundo saldo: ${saldoAtual}`);
-// mais um deposito
-conta1.depositar(50);
-saldoAtual = conta1.consultar();
-console.log(`terceiro saldo: ${saldoAtual}`);
-// mais um deposito
-conta1.depositar(200);
-saldoAtual = conta1.consultar();
-console.log(`saldo total ${saldoAtual}`);
+console.log(`Consulta ao saldo depois do deposito: ${saldoAtual}`);
 
 /**sacar */
 conta1.sacar(30);
 saldoAtual = conta1.consultar();
-console.log(`saldo atual depois do saque: ${saldoAtual}`);
-// mais um saque
-conta1.sacar(30);
-saldoAtual = conta1.consultar();
-console.log(`segundo saque: ${saldoAtual}`);
+console.log(`Saldo atual depois do saque: ${saldoAtual}`);
 
-console.log(`total da conta: ${saldoAtual}`);
+console.log(`Saldo total da conta: ${saldoAtual}`);
