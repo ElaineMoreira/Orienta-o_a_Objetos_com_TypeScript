c35d88b01afc2a6a10a27306d85b45217d2d938d
Elaine <elainefelipemoreira@gmail.com>
2022-03-05 10:51:47 -0300

    Adiciona um personagem

--- src/aula01-classes-construtores/Personagem.ts
+++ src/aula01-classes-construtores/Personagem.ts
@@ -1,3 +1,4 @@
+/**Cria classe */
 class Personagem {
   nome: string = "";
   energia: number = 0;
@@ -5,3 +6,12 @@ class Personagem {
   ataque: number = 0;
   defesa: number = 0;
 }
+/**Posso instaciar para criar novo personagem*/
+let sansa: Personagem;
+sansa = new Personagem();
+sansa.nome = "Sansa Stark";
+sansa.ataque = 10;
+sansa.defesa = 20;
+sansa.energia = 50;
+sansa.vida = 100;
+console.log("personagem :>>", sansa);
