4a74b0cdea63ff4a813b727f60786b00928d8097
Elaine <elainefelipemoreira@gmail.com>
2022-03-07 10:08:04 -0300

    Adiciona constructor com valores iniciais de 50 de ataque e energia

--- src/aula01-classes-construtores/Personagem.ts
+++ src/aula01-classes-construtores/Personagem.ts
@@ -5,16 +5,40 @@ class Personagem {
   vida: number = 0;
   ataque: number = 0;
   defesa: number = 0;
+
+  /**cria constructor para especificar como os meu atributos serao inicializados*/
+  /**define que todos os personagens criados inicializarão com valor de energia e ataque 50 */
+  constructor() {
+    this.energia = 50;
+    this.ataque = 50;
+  }
 }
-/**Posso instaciar para criar novo personagem*/
+
 let sansa: Personagem;
 sansa = new Personagem();
 sansa.nome = "Sansa Stark";
-sansa.ataque = 10;
-sansa.defesa = 20;
-sansa.energia = 50;
-sansa.vida = 100;
-console.log("personagem :>>", sansa);
+console.log("personagem: >>", sansa);
+
+/**saida
+ 
+  personagem :>> {
+   sansa.nome = "Sansa Stark";
+   sansa.ataque = 50;
+   sansa.defesa = 0;
+   sansa.energia = 50;
+   sansa.vida = 0;
+
+ */
+
+/**Posso instaciar para criar novo personagem*/
+// let sansa: Personagem;
+// sansa = new Personagem();
+// sansa.nome = "Sansa Stark";
+// sansa.ataque = 10;
+// sansa.defesa = 20;
+// sansa.energia = 50;
+// sansa.vida = 100;
+// console.log("personagem :>>", sansa);
 /**saida
 
  * personagem :>> {
@@ -26,14 +50,14 @@ console.log("personagem :>>", sansa);
    
  * }
  */
-let aria: Personagem;
-aria = new Personagem();
-aria.nome = "Ária Stark";
-aria.ataque = 100;
-aria.defesa = 80;
-aria.energia = 90;
-aria.vida = 150;
-console.log("personagem: >>", aria);
+// let aria: Personagem;
+// aria = new Personagem();
+// aria.nome = "Ária Stark";
+// aria.ataque = 100;
+// aria.defesa = 80;
+// aria.energia = 90;
+// aria.vida = 150;
+// console.log("personagem: >>", aria);
 /**saida
 
  * personagem :>> {
@@ -47,10 +71,10 @@ console.log("personagem: >>", aria);
  */
 
 /**forma diferente de contruir um personagem, chamando pelo contrutor padrao*/
-let jonh: Personagem = new Personagem();
-jonh.nome = "Jonh Snow";
-jonh.ataque = 80;
-jonh.defesa = 70;
-jonh.energia = 90;
-jonh.vida = 40;
-console.log("personagem: >>", jonh);
+// let jonh: Personagem = new Personagem();
+// jonh.nome = "Jonh Snow";
+// jonh.ataque = 80;
+// jonh.defesa = 70;
+// jonh.energia = 90;
+// jonh.vida = 40;
+// console.log("personagem: >>", jonh);
