414a61752687dc2fe0305548f49d886f3f9743ee
Elaine <elainefelipemoreira@gmail.com>
2022-03-07 09:52:48 -0300

    Cria personagem aria

--- src/aula01-classes-construtores/Personagem.ts
+++ src/aula01-classes-construtores/Personagem.ts
@@ -15,3 +15,36 @@ sansa.defesa = 20;
 sansa.energia = 50;
 sansa.vida = 100;
 console.log("personagem :>>", sansa);
+/**saida
+
+ * personagem :>> {
+ * sansa.nome = "Sansa Stark";
+   sansa.ataque = 10;
+   sansa.defesa = 20;
+   sansa.energia = 50;
+   sansa.vida = 100;
+   
+ * }
+ */
+let aria: Personagem;
+aria = new Personagem();
+aria.nome = "Ária Stark";
+aria.ataque = 100;
+aria.defesa = 80;
+aria.energia = 90;
+aria.vida = 150;
+console.log("personagem: >>", aria);
+/**saida
+
+ * personagem :>> {
+ * aria.nome = "Aria Stark";
+   aria.ataque = 100;
+   aria.defesa = 80;
+   aria.energia = 90;
+   aria.vida = 150;
+   
+ * }
+ */
+
+/**forma diferente de contruir um personagem, chamando pelo contrutor padrao*/
+
